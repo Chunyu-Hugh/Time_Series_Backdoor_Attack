783663f06b87378b7e02cb0363534d9759400f7e
Chunyu HU <hunao1995@gmail.com>
2024-04-19 16:05:20 -0400

    change some files

---
+++ run.sh
@@ -0,0 +1,22 @@
+python main.py run_baseline
+
+
+# To run the vanilla backdoor method:
+# ```
+python main.py run_backdoor vanilla
+# ```
+
+# To run the static noise backdoor method:
+# ```
+python main.py run_backdoor powerline
+# ```
+
+# To run our proposed TSBA:
+# ```
+python main.py run_backdoor generator
+# ```
+
+# To test the generator from trained TSBA model:
+# ```
+python main.py run_backdoor generative_test
+# ```
